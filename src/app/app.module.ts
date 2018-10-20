94b18c226141b88207b9a94153aa64953a2779f2
sis0k0 <stanimira.vlaeva@gmail.com>
2018-10-20 19:50:36 +0300

    add webpack and remove scaffold components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,9 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
+
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
-// Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
-
-// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
-// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     bootstrap: [
@@ -23,17 +15,10 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
-    ],
-    providers: [
-        ItemService
+        HomeComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
-/*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
 export class AppModule { }
