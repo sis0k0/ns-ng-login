60ca6c7ef4fdcbafba95e0528140571cffafba09
sis0k0 <stanimira.vlaeva@gmail.com>
2018-11-13 17:15:19 +0200

    add demo stuff

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,26 @@ import { AppOptions } from "nativescript-angular/platform-common";
 
 import { AppModule } from "./app/app.module";
 
-import persistState from "./persist-state";
-persistState();
+// import persistRoute from "./persist-route";
+// persistRoute();
 
-let options: AppOptions = {};
+// import persistState from "./persist-state";
+// persistState();
+
+const options: AppOptions = {};
 if (module['hot']) {
     const hmrUpdate = require("nativescript-dev-webpack/hmr").hmrUpdate;
 
     options.hmrOptions = {
         moduleTypeFactory: () => AppModule,
         livesyncCallback: (platformReboot) => {
-            console.log("HMR: Sync...")
             hmrUpdate();
             setTimeout(platformReboot, 0);
         },
     }
     hmrUpdate();
 
-    // Path to your app module.
-    // You might have to change it if your module is in a different place.
     module['hot'].accept(["./app/app.module"])
 }
-
 
 platformNativeScriptDynamic(options).bootstrapModule(AppModule);
